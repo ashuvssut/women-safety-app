159b82ae7b8d402537fbce8885a44f79361cdf43
ashuvssut <ashu.khanduala@gmail.com>
2020-12-11 22:39:33 +0530

    views adding

---
+++ lib/views/articles.dart
@@ -0,0 +1,17 @@
+// import 'package:flutter/material.dart';
+
+// class SignIn extends StatefulWidget {
+//   @override
+//   _SignInState createState() => _SignInState();
+// }
+
+// class _SignInState extends State<SignIn> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("With You - Women Safety App"),
+//       ),
+//     );
+//   }
+// }
