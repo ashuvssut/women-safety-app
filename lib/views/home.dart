48b949784aa50613b23465a3c1af2a05ecf3181e
ashuvssut <ashu.khanduala@gmail.com>
2020-12-12 14:05:07 +0530

    UI upd

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,6 @@
 import "package:flutter/material.dart";
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:women_safety_app/views/articles.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -8,9 +10,47 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
-        title: Text("With You - Home"),
+        title: Text("With You"),
+      ),
+      body: Row(
+        children: [
+          RaisedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Articles()),
+              );
+            },
+            shape: RoundedRectangleBorder(
+              side: BorderSide(
+                color: Colors.black45,
+              ),
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            child: Container(
+              width: 100,
+              height: 100,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SvgPicture.asset(
+                      "assets/icons/law.svg",
+                      height: size.height * 0.05,
+                    ),
+                  ),
+                  Text(
+                    "Laws Realted to Women",
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/views/signin.dart
+++ lib/views/signin.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:women_safety_app/services/auth.dart';
 
 class SignIn extends StatefulWidget {
@@ -9,16 +10,57 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
-        title: Text("With You SignIn"),
+        centerTitle: true,
+        title: Column(
+          children: [
+            Text("Sign in"),
+          ],
+        ),
       ),
+      backgroundColor: Colors.blue,
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            AuthMethods().signInWithGoogle(context);
-          }, 
-          child: Text("Sign In with Google")),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: 100.0),
+              child: SvgPicture.asset(
+                "assets/icons/happy.svg",
+                height: size.height * 0.35,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 50.0),
+              width: 250,
+              child: ButtonTheme(
+                buttonColor: Colors.white70,
+                child: RaisedButton(
+                  onPressed: () {
+                    AuthMethods().signInWithGoogle(context);
+                  },
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(38.0),
+                  ),
+                  child: Row(children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SvgPicture.asset(
+                        "assets/icons/google.svg",
+                        height: size.height * 0.05,
+                      ),
+                    ),
+                    Text(
+                      "Sign In with Google",
+                      textAlign: TextAlign.center,
+                    ),
+                  ]),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
