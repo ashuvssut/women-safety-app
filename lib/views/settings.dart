6f3cae9f7a7a6ec74bc396e1a326299cc2ab5bfb
ashuvssut <ashu.khanduala@gmail.com>
2020-12-13 09:30:31 +0530

    first commit

---
+++ lib/views/settings.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatefulWidget {
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(),
+    );
+  }
+}
